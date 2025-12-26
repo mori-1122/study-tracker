807a1036c66ede7a899406decfeb0ef0ba7a3ee8
Chiba <chi.11220214.yo@gmail.com>
2025-12-26 23:46:35 +0900

    rails g時に不要なファイルが自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -15,13 +15,9 @@ module StudyTracker
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.assets false
+      g.test_framework :rspec
+    end
   end
 end
