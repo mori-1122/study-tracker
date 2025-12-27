be153c05362c94c1712191533f50d425e8a3d38a
Chiba <chi.11220214.yo@gmail.com>
2025-12-27 20:53:44 +0900

    トップページのリクエストテストを修正

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper'
+require 'rails_helper' # rails_helperを読み込み
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /top" do
-    it "returns http success" do
-      get "/home/top"
-      expect(response).to have_http_status(:success)
+RSpec.describe "Home", type: :request do # テスト対象モジュールと、テストの種類
+  describe "GET /" do # テストに関すつ説明
+    it "トップページを開くと「成功」の返事が返る" do # 期待する結果
+      get "/" # どういうパスにgetリクエストを投げる
+      expect(response).to have_http_status(200) # 結果
     end
   end
 end
