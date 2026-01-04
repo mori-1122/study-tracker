9c50f9b796872842665403903fa4d5ca07b71fca
Chiba <chi.11220214.yo@gmail.com>
2026-01-04 21:38:09 +0900

    投稿作成に認証を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,34 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
   def index
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new
+    @post.user_id = current_user.id
+
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to root_path
+    else
+      flash[:notice] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def strong_params
+    params.expect(posts: [ :title, :content ])
   end
 end
