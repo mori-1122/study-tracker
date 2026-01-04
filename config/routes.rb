0a5c96be0512af2d3bd6d95665c754db3b735344
Chiba <chi.11220214.yo@gmail.com>
2026-01-04 20:40:59 +0900

    postsコントローラに関するファイルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/new"
+  get "posts/create"
+  get "posts/show"
+  get "posts/destroy"
   devise_for :users
   root "home#top"
 end
