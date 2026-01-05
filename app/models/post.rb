c36df19e79b71ecd3723e0482df5c7de48c1aad7
Chiba <chi.11220214.yo@gmail.com>
2026-01-05 10:00:34 +0900

    Action Text関連のファイルを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 1000 }
+  has_rich_text :content
 end
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./app/views/**/*.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js"
+  ],
+  corePlugins: {
+    preflight: false
+  }
+}
